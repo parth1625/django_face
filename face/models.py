8b710f73c7eaa849a74ea3fe8ac9f79bfa3d6721
Parth Vijay <parth@securitytroops.com>
2020-02-20 18:07:14 +0530

    change in models.py

--- face/models.py
+++ face/models.py
@@ -5,5 +5,5 @@ from django.conf import settings
 # Create your models here.
 class Opencv(models.Model):
 	title = models.CharField(max_length=200)
-	file_input = models.ImageField(upload_to='images/', blank=True)
+	file_input = models.ImageField(upload_to='images/')
 	file_output = models.ImageField(blank=True)
